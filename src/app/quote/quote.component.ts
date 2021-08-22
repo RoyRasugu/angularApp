562824a23777348eb9b4d00d3e6109586661f502
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-08-23 00:55:19 +0300

    alter dates

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
      quotes:Quote[] = [
-      new Quote(1, 'Charles', 'Living Life', 'Live as if you were to die tomorrow. Learn as if you were to live forever','Milton Berle',new Date(2021,3,14),0,0),
+      new Quote(1, 'Charles', 'Living Life', 'Live as if you were to die tomorrow. Learn as if you were to live forever','Milton Berle',new Date(2021,7,23),0,0),
       new Quote(2, 'Katie', 'Personal Feelings', 'Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind','Napoleon Hill',new Date(2021,6,17),0,0),
       new Quote(3, 'Catherine', 'Human Perceptions', 'We must not allow other people’s limited perceptions to define us.','Oscar Wilde',new Date(2021,1,12),0,0),
       new Quote(4, 'Wendell', 'Greatness', 'If you cannot do great things, do small things in a great way','Virginia Satir',new Date(2021,2,14),0,0),
-      new Quote(5, 'Carter', 'Important Wisdom', 'Wise men speak because they have something to say; fools because they have to say something','Bernard M. Baruch',new Date(2021,3,14),0,0),
+      new Quote(5, 'Carter', 'Important Wisdom', 'Wise men speak because they have something to say; fools because they have to say something','Bernard M. Baruch',new Date(2021,7,14),0,0),
       new Quote(6, 'Petra', 'Success', 'Strive not to be a success, but rather to be of value.','Lao Tzu',new Date(2021,1,30),0,0)
      ];
      get sortQuotes() {
